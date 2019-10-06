3f5703a40fde3fd13e6d14e3a96915579bc1f6e8
Zoe <zoekharvey@gmail.com>
2019-10-06 11:56:06 -0600

    Fix jest files and pass first test

--- src/components/NewsArticle/NewsArticle.test.js
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NewsArticle from './NewsArticle';
+
+describe('NewsArticle', () => {
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(
+      <NewsArticle
+        key={1}
+        id={1}
+        headline="This headline"
+        image="This Image"
+        description="This description"
+        url="https://www.thetests.com"
+      />
+    )
+    expect(wrapper).toMatchSnapshot();
+  })
+})
