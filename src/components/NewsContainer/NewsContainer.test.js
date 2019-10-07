c0f2e0ec3fc07bb99f7d3822337f35ae0d10227c
Zoe <zoekharvey@gmail.com>
2019-10-06 20:17:31 -0600

    Add passing test for NewsContainer snapshot

--- src/components/NewsContainer/NewsContainer.test.js
+++ src/components/NewsContainer/NewsContainer.test.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import NewsContainer from './NewsContainer';
 import { shallow } from 'enzyme';
+import local from "../../data/local"
 
+describe("NewsContainer", () => {
+  it("should match the snapshot", () => {
+    const wrapper = shallow(<NewsContainer currentSubject={local} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
