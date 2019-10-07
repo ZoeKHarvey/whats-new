d327d9664686b381637a11369435e38fc55aa4ac
Zoe <zoekharvey@gmail.com>
2019-10-06 20:52:06 -0600

    Updates

---
+++ precache-manifest.cab1bc74f9bc8fe5e1af216cc67f1bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae978e20dfede0b9c1974095e728f24c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "438307448d9262603852",
+    "url": "/static/css/main.905f7b7a.chunk.css"
+  },
+  {
+    "revision": "409a9630974803ba81db",
+    "url": "/static/js/2.aa69acc4.chunk.js"
+  },
+  {
+    "revision": "438307448d9262603852",
+    "url": "/static/js/main.50adbce2.chunk.js"
+  },
+  {
+    "revision": "25612ba57795dcc22c86",
+    "url": "/static/js/runtime-main.ae1c00e5.js"
+  },
+  {
+    "revision": "269bffbdfcfb823e0316b6d3b74cecde",
+    "url": "/static/media/right-arrow.269bffbd.svg"
+  }
+]);
