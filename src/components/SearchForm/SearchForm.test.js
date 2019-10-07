6aa264246755972357c08046a6c2cdb4583aeb76
Zoe <zoekharvey@gmail.com>
2019-10-06 19:11:57 -0600

    Add passing test for search function

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -4,15 +4,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 describe('SearchForm', () => {
-  const searched = jest.fn();
+  const searchArticle = jest.fn();
 
-  it('should match the snapshot information passed correctly', () => {
+  it('should match the snapshot information', () => {
     const wrapper = shallow(
       <SearchForm
-        searched={searched}
+        searchArticle={searchArticle}
       />
     )
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should call the searchArticle method article on change', () => {
+    const wrapper = shallow(
+      <SearchForm
+        searchArticle={searchArticle}
+      />
+    )
+    
+    const mockInput = { target: { value: 'Stuff' } };
+
+    wrapper.find('input').simulate('change', mockInput);
+    expect(searchArticle).toHaveBeenCalled();
   })
 
 })
